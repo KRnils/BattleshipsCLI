2f45769e97e2a2e097bbee63908785ad83aaedb6
Robert Nilsson <krnilsson@yandex.com>
2024-05-03 17:17:50 +0100

    Switch solution to curses library to make output much nicer

--- run.py
+++ run.py
@@ -2,13 +2,20 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from random import randint
-from pprint import pprint
+import curses
+from curses import wrapper
+
 
 class Board:
+    """
+    Board class that holds the current status of a players board (both computer and player)
+    """
 
     def __init__(self, size):
+        self.ships = 5
+        self.size = size
         self.grid = [['.' for i in range(size)]]*size
-    
+
     def printSelf(self):
         row = ""
         for i in self.grid:
@@ -17,14 +24,46 @@ class Board:
             print(row)
             row = ""
 
-def main(size):
-    player_board = Board(size=size)
-    computer_board = Board(size=size)
-    player_board.printSelf()
+    def add_ship(x,y):
+        pass
+
+    def check_hit():
+        print(randint(0,10))
+        pass
 
 
+def main(stdscr):
+    """
+    Main game loop runs here
+    """
+    # Draw the board
+    size = 10
+    board = ["."*size for i in range(0, size)]
+    board += ["-"*size]
+    board += ["."*size for i in range(0, size)]
+    
+    start_y, start_x = 8, 2  # Start position of the cursor (based on size)
 
+    # initialize curses
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
 
+    # game logic 
+    for i in range(0, size):
+        for j in range(0, size):
+            stdscr.addstr(i, j, ".")
+    
+    playing = True
+    while (playing):
+        stdscr.refresh()
+        stdscr.getkey()
 
-if __name__ == '__main__':
-    main(size=7)
+    # end game logic and revert curses terminal settings
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+wrapper(main)
