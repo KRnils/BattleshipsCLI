e3fc9946325dc4b9944c64b903b8d38c14f93ef3
Robert Nilsson <krnilsson@yandex.com>
2024-05-03 13:15:41 +0100

    Add Board class and function to print board, set up mock board for player and computer

--- run.py
+++ run.py
@@ -2,5 +2,29 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from random import randint
+from pprint import pprint
 
-print(randint(1, 100))
+class Board:
+
+    def __init__(self, size):
+        self.grid = [['.' for i in range(size)]]*size
+    
+    def printSelf(self):
+        row = ""
+        for i in self.grid:
+            for j in i:
+                row += ' '+(j)
+            print(row)
+            row = ""
+
+def main(size):
+    player_board = Board(size=size)
+    computer_board = Board(size=size)
+    player_board.printSelf()
+
+
+
+
+
+if __name__ == '__main__':
+    main(size=7)
