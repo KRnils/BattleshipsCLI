409b1c50b5fa0d00cf66e0d0cf75200791c516f2
Robert Nilsson <krnilsson@yandex.com>
2024-05-05 15:16:19 +0100

    Add attack function, tidy upp intermission phase

--- run.py
+++ run.py
@@ -13,18 +13,46 @@ class Board:
     Board class that holds the current status of the game board
     """
 
-    def __init__(self, size):
+    def __init__(self, size, ships):
+        """
+        Initialize board based on board size and ship count
+        """
         self.size = size
         self.grid = [list("."*size) for i in range(0, size)]
         self.grid += [list("-"*size)]
         self.grid += [list("."*size) for i in range(0, size)]
-
-    def getStrings(self):
-        return self.grid
+        self.hidden_ships = []
+        while (len(self.hidden_ships) < ships):
+            tup = (randint(0, size), randint(0, size))
+            if tup not in self.hidden_ships:
+                self.hidden_ships.append(tup)
 
     def add_ship(self, y, x):
+        """
+        For adding ships during the setup phase.
+        If the spot is empty, add ship and return True
+        """
         if self.grid[y][x] == ".":
             self.grid[y][x] = "§"
+            return True
+        else:
+            return False
+    
+    def attack_pos(self, y, x):
+        """
+        Check for a hit, update the grid to show an x if there's a ship there and return True.
+        If there is no ship there update the grid to show a zero.
+        If that spot has already been attacked return False.
+        """
+        if self.grid[y][x] == ".":
+            if (y, x) in self.hidden_ships:
+                self.grid[y][x] == "x"
+                return "hit"
+            else:
+                self.grid[y][x] == "0"
+                return "miss"
+        else:
+            return False
 
     def check_hit():
         print(randint(0, 10))
@@ -33,10 +61,10 @@ class Board:
 
 def redraw(window, board):
     """
-    Redraws the game area, this will be called for every input or update to
+    Redraws the game area, this should be called for every input or update to
     the board.
     """
-
+    window.clear()
     for i in range(0, len(board.grid)):
         for j in range(0, len(board.grid[0])):
             window.addstr(i, j, str(board.grid[i][j]))
@@ -53,21 +81,21 @@ def start_game(window):
     cursor_x, cursor_y = 0, board_size*2
 
     # Create the board
-    board = Board(size=board_size)
+    board = Board(size=board_size, ships=ships_count)
 
     # Game setup phase
     placed_ships = 0
     playing = True
-    window.addstr(0, board_size+2, "Place your ships")
     while (placed_ships < ships_count and playing):
         redraw(window, board)
         window.move(cursor_y, cursor_x)
+        window.addstr(0, board_size+2, "Place your ships")
         window.addstr(
             1, board_size+2,
             "Remaing ships "+str(ships_count-placed_ships))
         action = window.getkey(cursor_y, cursor_x)
         if action == "q":
-            playing = False
+            return
         elif action == "KEY_UP":
             if (cursor_y > board_size+1):
                 cursor_y -= 1
@@ -80,55 +108,54 @@ def start_game(window):
         elif action == "KEY_RIGHT":
             if (cursor_x < board_size-1):
                 cursor_x += 1
-        elif action == "\n":
+        elif action == "\n" or action == " ":
             action_location = window.getyx()
             window.addstr(3, board_size+2, str(action_location))
-            board.add_ship(*action_location)
-            placed_ships += 1
-        
-        window.addstr(
-            1, board_size+2,
-            "Remaing ships "+str(ships_count-placed_ships))
-    
+            if board.add_ship(*action_location):
+                placed_ships += 1
+            else:
+                pass
+        window.clear()
+
     # Intermission phase
-    window.refresh()
-    sleep(1)
     curses.curs_set(0)
+    redraw(window, board)
     window.addstr(0, board_size+2, "Ships placed, get ready for battle!")
     window.refresh()
     sleep(2.5)
-    window.clear()    
 
-    # Set up board and cursor for playing phase
-    window.addstr(0, board_size+2, "Select a position to attack")
+    # Set up board and cursor for battle phase
     curses.curs_set(2)
     cursor_y, cursor_x = 0, 0
 
+    # battle phase
     while (playing):
         redraw(window, board)
         window.move(cursor_y, cursor_x)
-        try:
-            action = window.getkey(cursor_y, cursor_x)
-            if action == "q":
-                playing = False
-            elif action == "KEY_UP":
-                if (cursor_y > 0):
-                    cursor_y -= 1
-            elif action == "KEY_DOWN":
-                if (cursor_y < board_size-1):
-                    cursor_y += 1
-            elif action == "KEY_LEFT":
-                if (cursor_x > 0):
-                    cursor_x -= 1
-            elif action == "KEY_RIGHT":
-                if (cursor_x < board_size-1):
-                    cursor_x += 1
-            elif action == "\n":
-                action_location = window.getyx()
-                window.addstr(3, board_size+2, str(action_location))
-        except ValueError as error:   # does nothing right now :p
-            print(error)
-            break
+        window.addstr(0, board_size+2, "Select a position to attack")
+        action = window.getkey(cursor_y, cursor_x)
+        if action == "q":
+            playing = False
+        elif action == "KEY_UP":
+            if (cursor_y > 0):
+                cursor_y -= 1
+        elif action == "KEY_DOWN":
+            if (cursor_y < board_size-1):
+                cursor_y += 1
+        elif action == "KEY_LEFT":
+            if (cursor_x > 0):
+                cursor_x -= 1
+        elif action == "KEY_RIGHT":
+            if (cursor_x < board_size-1):
+                cursor_x += 1
+        elif action == "\n" or action == " ":
+            action_location = window.getyx()
+            if board.attack_pos(*action_location):
+                pass
+            else:
+                window.addstr(
+                            1, board_size+2,
+                            "Already attacked there!")
 
     # Undo changes to terminal output, might not be needed
     # since we have the wrapper.
@@ -139,6 +166,11 @@ def start_game(window):
 
 
 def main():
+    """
+    Sets up the game by asking the user for board size and ship count.
+    This is done before entering the curses wrapper,
+    so the terminal still behaves normally.
+    """
     global board_size
     global ships_count
     while (board_size < 3 or board_size > 12):
